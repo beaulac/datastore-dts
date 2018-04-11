c8f18dfb82418534acb991751472b7009f95efe6
Antoine Beauvais-Lacasse <antoine.beaulac@gmail.com>
2018-04-11 10:29:39 -0400

    Change Query to CommonJS-style module.

--- index.d.ts
+++ index.d.ts
@@ -25,34 +25,29 @@ declare module '@google-cloud/datastore' {
         CommitCallback,
         CommitResult,
     } from '@google-cloud/datastore/request';
-    import {
-        Query as DatastoreQuery,
-        MoreResultsAfterCursor,
-        MoreResultsAfterLimit,
-        NoMoreResults,
-    } from '@google-cloud/datastore/query';
     import { DatastoreTransaction } from '@google-cloud/datastore/transaction';
+    import Query = require('@google-cloud/datastore/query');
 
     class Datastore extends DatastoreRequest_ {
         constructor(options: InitOptions);
 
         readonly KEY: typeof KEY_SYMBOL;
-        readonly MORE_RESULTS_AFTER_CURSOR: MoreResultsAfterCursor;
-        readonly MORE_RESULTS_AFTER_LIMIT: MoreResultsAfterLimit;
-        readonly NO_MORE_RESULTS: NoMoreResults;
+        readonly MORE_RESULTS_AFTER_CURSOR: Query.MoreResultsAfterCursor;
+        readonly MORE_RESULTS_AFTER_LIMIT: Query.MoreResultsAfterLimit;
+        readonly NO_MORE_RESULTS: Query.NoMoreResults;
 
         static readonly KEY: typeof KEY_SYMBOL;
-        static readonly MORE_RESULTS_AFTER_CURSOR: MoreResultsAfterCursor;
-        static readonly MORE_RESULTS_AFTER_LIMIT: MoreResultsAfterLimit;
-        static readonly NO_MORE_RESULTS: NoMoreResults;
+        static readonly MORE_RESULTS_AFTER_CURSOR: Query.MoreResultsAfterCursor;
+        static readonly MORE_RESULTS_AFTER_LIMIT: Query.MoreResultsAfterLimit;
+        static readonly NO_MORE_RESULTS: Query.NoMoreResults;
 
-        static readonly Query: typeof DatastoreQuery;
+        static readonly Query: typeof Query;
         static readonly DatastoreRequest: typeof DatastoreRequest_;
         static readonly Transaction: typeof DatastoreTransaction;
 
         // tslint:disable-next-line unified-signatures (Arg is semantically different)
-        createQuery(namespace: string, kind: string): DatastoreQuery;
-        createQuery(kind: string): DatastoreQuery;
+        createQuery(namespace: string, kind: string): Query;
+        createQuery(kind: string): Query;
 
         save(entities: OneOrMany<object>, callback: CommitCallback): void;
         save(entities: OneOrMany<object>): Promise<CommitResult>;
@@ -164,20 +159,23 @@ declare module '@google-cloud/datastore/entity' {
 declare module '@google-cloud/datastore/query' {
     // tslint:disable-next-line no-duplicate-imports (This rule is broken for multiple modules per file)
     import { DatastoreKey } from '@google-cloud/datastore/entity';
+    import { DatastoreRequest } from '@google-cloud/datastore/request';
 
-    type MoreResultsAfterCursor = 'MORE_RESULTS_AFTER_CURSOR';
-    type MoreResultsAfterLimit = 'MORE_RESULTS_AFTER_LIMIT';
-    type NoMoreResults = 'NO_MORE_RESULTS';
+    export = Query;
 
     class Query {
-        constructor(scope: string, kinds: string, namespace: string);
+        scope: DatastoreRequest;
+        kinds: string;
+        namespace?: string;
 
-        filter(property: string, operator: QueryFilterOperator, value: any): this;
+        constructor(scope: DatastoreRequest, kinds: string, namespace: string);
+
+        filter(property: string, operator: Query.QueryFilterOperator, value: any): this;
         filter(property: string, value: any): this;
 
         hasAncestor(key: DatastoreKey): this;
 
-        order(property: string, options?: OrderOptions): this;
+        order(property: string, options?: Query.OrderOptions): this;
 
         groupBy(properties: string | ReadonlyArray<string>): this;
 
@@ -191,38 +189,45 @@ declare module '@google-cloud/datastore/query' {
 
         offset(n: number): this;
 
-        run(callback: QueryCallback): void;
-        run(options: QueryOptions, callback: QueryCallback): void;
-        run(options?: QueryOptions): Promise<QueryResult>;
+        run(callback: Query.QueryCallback): void;
+        run(options: Query.QueryOptions, callback: Query.QueryCallback): void;
+        run(options?: Query.QueryOptions): Promise<Query.QueryResult>;
 
         runStream(): NodeJS.ReadableStream;
     }
 
-    type QueryFilterOperator = '<' | '<=' | '=' | '>=' | '>';
+    namespace Query {
+        type QueryFilterOperator = '<' | '<=' | '=' | '>=' | '>';
 
-    interface OrderOptions {
-        descending: boolean;
+        interface OrderOptions {
+            descending?: boolean;
+        }
+
+        interface QueryOptions {
+            consistency?: 'strong' | 'eventual';
+            maxApiCalls?: number;
+        }
+
+        type MoreResultsAfterCursor = 'MORE_RESULTS_AFTER_CURSOR';
+        type MoreResultsAfterLimit = 'MORE_RESULTS_AFTER_LIMIT';
+        type NoMoreResults = 'NO_MORE_RESULTS';
+
+        interface QueryInfo {
+            endCursor?: string;
+            readonly moreResults: MoreResultsAfterCursor | MoreResultsAfterLimit | NoMoreResults;
+        }
+
+        type QueryCallback = (err: Error, entities: object[], info: QueryInfo) => void;
+        type QueryResult = [object[], QueryInfo];
     }
-
-    interface QueryOptions {
-        consistency?: 'strong' | 'eventual';
-        maxApiCalls?: number;
-    }
-
-    interface QueryInfo {
-        endCursor?: string;
-        readonly moreResults: MoreResultsAfterCursor | MoreResultsAfterLimit | NoMoreResults;
-    }
-
-    type QueryCallback = (err: Error, entities: object[], info: QueryInfo) => void;
-    type QueryResult = [object[], QueryInfo];
 }
 
 declare module '@google-cloud/datastore/request' {
     // tslint:disable-next-line no-duplicate-imports (This rule is broken for multiple modules per file)
     import { DatastoreKey, OneOrMany } from '@google-cloud/datastore/entity';
-    // tslint:disable-next-line no-duplicate-imports
-    import { Query, QueryCallback, QueryOptions, QueryResult } from '@google-cloud/datastore/query';
+    import Query = require('@google-cloud/datastore/query');
+    import QueryOptions = Query.QueryOptions;
+    import QueryCallback = Query.QueryCallback;
 
     /**
      * Creates requests to the Datastore endpoint.
@@ -236,7 +241,6 @@ declare module '@google-cloud/datastore/request' {
                          options: QueryOptions): NodeJS.ReadableStream;
 
         delete(keyOrKeys: DatastoreKey | ReadonlyArray<DatastoreKey>, callback: CommitCallback): void;
-        // tslint:disable-next-line void-return (Rule seems broken: this IS a return value)
         delete(keyOrKeys: DatastoreKey | ReadonlyArray<DatastoreKey>): Promise<CommitResult> | void;
 
         get(key: DatastoreKey, options: QueryOptions, callback: GetCallback<object>): void;
@@ -249,22 +253,22 @@ declare module '@google-cloud/datastore/request' {
 
         runQuery(query: Query, options: QueryOptions, callback: QueryCallback): void;
         runQuery(query: Query, callback: QueryCallback): void;
-        runQuery(query: Query, options?: QueryOptions): Promise<QueryResult>;
+        runQuery(query: Query, options?: QueryOptions): Promise<Query.QueryResult>;
 
         runQueryStream(query: Query, options?: QueryOptions): NodeJS.ReadableStream;
 
-        save(entities: OneOrMany<object>, callback: CommitCallback): void;
+        save(entities: OneOrMany, callback: CommitCallback): void;
         // tslint:disable-next-line void-return (Rule seems broken: this IS a return value)
-        save(entities: OneOrMany<object>): Promise<CommitResult> | void;
+        save(entities: OneOrMany): Promise<CommitResult> | void;
 
-        insert(entities: OneOrMany<object>, callback: CommitCallback): void;
-        insert(entities: OneOrMany<object>): Promise<CommitResult>;
+        insert(entities: OneOrMany, callback: CommitCallback): void;
+        insert(entities: OneOrMany): Promise<CommitResult>;
 
-        update(entities: OneOrMany<object>, callback: CommitCallback): void;
-        update(entities: OneOrMany<object>): Promise<CommitResult>;
+        update(entities: OneOrMany, callback: CommitCallback): void;
+        update(entities: OneOrMany): Promise<CommitResult>;
 
-        upsert(entities: OneOrMany<object>, callback: CommitCallback): void;
-        upsert(entities: OneOrMany<object>): Promise<CommitResult>;
+        upsert(entities: OneOrMany, callback: CommitCallback): void;
+        upsert(entities: OneOrMany): Promise<CommitResult>;
     }
 
     interface MutationResult {
@@ -291,8 +295,7 @@ declare module '@google-cloud/datastore/transaction' {
     import Datastore_ = require('@google-cloud/datastore');
     // tslint:disable-next-line no-duplicate-imports (This rule is broken for multiple modules per file)
     import { DatastoreKey, OneOrMany } from '@google-cloud/datastore/entity';
-    // tslint:disable-next-line no-duplicate-imports
-    import { Query } from '@google-cloud/datastore/query';
+    import Query = require('@google-cloud/datastore/query');
     // tslint:disable-next-line no-duplicate-imports
     import { DatastoreRequest, CommitCallback, CommitResult } from '@google-cloud/datastore/request';
 
